4f75d1b7d4daf254623ff111c81e4b1d8a3ac3e0
PriyobrotoKarmakar <karmakarpriyobroto@gmail.com>
2024-12-29 18:27:53 +0530

    update2

--- DAY2/args.java
+++ DAY2/args.java
@@ -1,9 +1,8 @@
 package DAY2;
 
 public class args {
-        public static void main(String[] args) {
-            System.out.println(args[0]);
-        }
-    
-    
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+    }
+
 }
