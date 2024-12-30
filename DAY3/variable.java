b0fe498fb1c16cebadf09b16ea94d4dac52923fe
PriyobrotoKarmakar <karmakarpriyobroto@gmail.com>
2024-12-30 21:04:48 +0530

    update

---
+++ DAY3/variable.java
@@ -0,0 +1,16 @@
+package DAY3;
+
+public class variable {
+    public static void main(String[] args) {
+        int a = 5;
+        char ch = 'A';
+        int value = ch;
+        double d = 5.5;
+        int d1 = (int) d;
+
+        System.out.println(value);
+        System.out.println(d);
+        System.out.println(d1);
+        System.out.println(ch);
+    }
+}
