89147f9eb79b14432e81c6ac007c45390575cf20
PriyobrotoKarmakar <karmakarpriyobroto@gmail.com>
2025-01-18 12:46:07 +0530

    update for  neo colab

--- 0_Neo_Colab_Ans/UNIT 1/Lec7/q6.java
+++ 0_Neo_Colab_Ans/UNIT 1/Lec7/q6.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class q6 {
     // You are using Java
-    public static void main(String args[]) {
+    // You are using Java
+    public static void main(String[] args) {
         // Type your code here
         Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
+        double salary1 = input.nextDouble();
+        double salary2 = input.nextDouble();
+        double salary3 = input.nextDouble();
 
-        System.out.printf("Square of %d is %d\n", num, num * num);
-        System.out.printf("Cube of %d is %d\n", num, num * num * num);
+        double totalSalary = salary1 + salary2 + salary3;
+
+        System.out.printf("Rs. %.2f", totalSalary);
     }
 }
