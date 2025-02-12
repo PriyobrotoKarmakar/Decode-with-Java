103e6186ee795a9643a2d5ffef7d4485ce94d373
PriyobrotoKarmakar <karmakarpriyobroto@gmail.com>
2025-02-12 21:07:43 +0530

    ggs

---
+++ 0_Neo_Colab_Ans/UNIT 2/Static/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    // You are using Java
+    public static void main(String[] args) {
+        // type your code here
+        Scanner input = new Scanner(System.in);
+        String sen = input.nextLine();
+        char[] str = sen.toCharArray();
+        System.out.print(Arrays.toString(str));
+    }
+}
+
+public class q5 {
+
+}
